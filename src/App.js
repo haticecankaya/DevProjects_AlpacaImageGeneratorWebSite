4bb9a9ade0b969c9590bfce4124b3e002678e5ec
Hatice Çankaya <81267955+haticecankaya@users.noreply.github.com>
2024-07-12 15:47:16 +0300

    "first"

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Main">
+      <h1>Alpaca Generator</h1>
+      <div className='Customize'>
+      <div className='Avatar'>
+        <h2>deneme</h2>
+      </div>
+      <div className='Options'>
+      <h2>deneme</h2>
+      </div>
+      </div>
+      <div className='Buttons'>
+        <button>Button1</button>
+        <button>Button2</button>
+      </div>
+      </div> 
+    </div>
+  );
+}
+
+export default App;
